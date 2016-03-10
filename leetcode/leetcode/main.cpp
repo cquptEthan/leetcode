cdf9889f251ebeeadbec201d70955ababcfdca77
ZhangEthan <228477911@qq.com>
2016-03-10 15:51:01 +0800

    init

---
+++ leetcode/leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by ZhangEthan on 16/3/10.
+//  Copyright © 2016年 ZhangEthan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
